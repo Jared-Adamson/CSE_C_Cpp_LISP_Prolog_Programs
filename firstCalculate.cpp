bf2d7136256552fc81c4f158e130359c304aebb5
Jared Adamson <jared.d.adamson@gmail.com>
2014-10-30 16:28:09 -0700

    Work from C++ Book

---
+++ firstCalculate.cpp
@@ -0,0 +1,15 @@
+//Jared Adamson
+//C++ Practice - from Begining C++
+// Calculate & Print Numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+         cout << "7 + 3 = " << 7 + 3 << endl;
+         cout << "7 - 3 = " << 7 - 3 << endl;
+         cout << "7 * 3 = " << 7 * 3 << endl;
+        
+         return 0;
+}
